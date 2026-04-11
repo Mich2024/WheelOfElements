d2e02c9d37fb162d8dda4840b1d7d6f0eb5f20aa
AMoedder <alexanderformell@gmail.com>
2026-04-11 16:51:47 +0200

    add redesigns

--- ToPdfPipeline/Parser_WheelOfFate.py
+++ ToPdfPipeline/Parser_WheelOfFate.py
@@ -581,19 +581,18 @@ if __name__ == "__main__":
     ### INPUT OF PROPER RACES AND CLASSES
     files_Input = []    
 
-    #files_Input.append(r"../Races/Centaur.txt")
-    #files_Input.append(r"../Races/Dragonblood.txt")
+    files_Input.append(r"../Races/Centaur.txt")
     files_Input.append(r"../Races/Dwarf.txt")
     #files_Input.append(r"../Races/Elf.txt")
 
-    #files_Input.append(r"../Races/Fae.txt")
-    #files_Input.append(r"../Races/Halfelf.txt")
-    #files_Input.append(r"../Races/Human.txt")
-    #files_Input.append(r"../Races/Merman.txt")
-    #files_Input.append(r"../Races/Silverkin.txt")
-    #files_Input.append(r"../Races/Solarian.txt")
-    #files_Input.append(r"../Races/Thyger.txt")
-    #files_Input.append(r"../Races/Wyrmkin.txt")
+    files_Input.append(r"../Races/Fae.txt")
+    files_Input.append(r"../Races/Halfelf.txt")
+    files_Input.append(r"../Races/Human.txt")
+    files_Input.append(r"../Races/Merman.txt")
+    files_Input.append(r"../Races/Silverkin.txt")
+    files_Input.append(r"../Races/Solarian.txt")
+    files_Input.append(r"../Races/Thyger.txt")
+    files_Input.append(r"../Races/Wyrmkin.txt")
     
     files_Input.append(r"../Classes/Toxicologist1.txt")
     files_Input.append(r"../Classes/Toxicologist2.txt")
